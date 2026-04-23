5acc30baae6256c094279a1e6758116bed250259
spx333 <spx3@ya.ru>
2026-04-23 17:30:35 +0300

    Initial Commit

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,19 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Сергей Петров on 23.04.2026.
+//
+
+import Testing
+@testable import Tracker
+
+struct TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
