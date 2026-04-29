50303f1b02f75ccce1809652a1c5fb10a2f863ec
spx333 <spx3@ya.ru>
2026-04-29 16:48:19 +0300

    Сверстан экран LaunchScreen, TrackersViewController и добавлены исходные данные

---
+++ Tracker/Models/Weekday.swift
@@ -0,0 +1,42 @@
+//
+//  Weekday.swift
+//  Tracker
+//
+//  Created by Сергей Петров on 24.04.2026.
+//
+
+import Foundation
+
+enum Weekday: Int, CaseIterable {
+    case sunday = 1
+    case monday
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+    case saturday
+    
+    var fullName: String {
+        switch self {
+        case .monday: "Понедельник"
+        case .tuesday: "Вторник"
+        case .wednesday: "Среда"
+        case .thursday: "Четверг"
+        case .friday: "Пятница"
+        case .saturday: "Суббота"
+        case .sunday: "Воскресенье"
+        }
+    }
+    
+    var shortName: String {
+        switch self {
+        case .monday: "Пн"
+        case .tuesday: "Вт"
+        case .wednesday: "Ср"
+        case .thursday: "Чт"
+        case .friday: "Пт"
+        case .saturday: "Сб"
+        case .sunday: "Вс"
+        }
+    }
+}
